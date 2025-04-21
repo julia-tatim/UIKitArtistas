c6d706be031d719ce2bf94c4b76f95b813e7a687
julia-tatim <juliatatimcastro@gmail.com>
2025-04-21 13:04:41 -0400

    Initial Commit

---
+++ UIKit Artistas Curitibanos/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  UIKit Artistas Curitibanos
+//
+//  Created by user275803 on 4/21/25.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ UIKit Artistas Curitibanos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Artistas Curitibanos
+//
+//  Created by user275803 on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
