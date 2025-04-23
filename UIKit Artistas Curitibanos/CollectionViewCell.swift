641aba42ebfd2644b0e38aba55b0683de2267d84
julia-tatim <juliatatimcastro@gmail.com>
2025-04-23 15:26:48 -0400

    Colocando itens no banco de dados e exibindo na pagina inicial

--- UIKit Artistas Curitibanos/CollectionViewCell.swift
+++ UIKit Artistas Curitibanos/CollectionViewCell.swift
@@ -12,4 +12,11 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var obraNomeLabel: UILabel!
     @IBOutlet weak var obraArtistaLabel: UILabel!
     @IBOutlet weak var obraImg: UIImageView!
+    
+    func setup(with obra: ObraDeArte) {
+        let imagem = UIImage(named: obra.imagemNome)
+        obraNomeLabel.text = obra.titulo
+        obraArtistaLabel.text = obra.artista
+        obraImg.image = imagem
+    }
 }
--- UIKit Artistas Curitibanos/ViewController.swift
+++ UIKit Artistas Curitibanos/ViewController.swift
@@ -12,17 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView.dataSource = self
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return obras.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.setup(with: obras[indexPath.row])
+        return cell
     }
 }
